7472bfdb75dc0d40bc22ce730c48d9e1f8781219
kavjeydev <kavin_jey@Kavins-MacBook-Pro-2.local>
2024-08-09 16:18:04 -0600

    set up django

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 
-client = OpenAI(api_key="sk-VMbVWoS5aF48-kDBDGc4yekmznV4YQgZ-wIj3sXZAOT3BlbkFJYVrwmEFTdfLnUufSctff_T6fp3G8lkdHh4iuT1zgYA")
+client = OpenAI()
 completion = client.chat.completions.create(
     model="gpt-4o",
     messages=[
