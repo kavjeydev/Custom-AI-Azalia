2b15941f13ad3aeb1f12f3df893f00f209a0ce8c
kavjeydev <kavin_jey@Kavins-MacBook-Pro-2.local>
2024-08-09 16:18:59 -0600

    g

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
 from openai import OpenAI
 
-client = OpenAI()
-completion = client.chat.completions.create(
-    model="gpt-4o",
-    messages=[
-        {"role": "user", "content": "write a haiku about ai"}
-    ]
-)
 
-for chunk in completion:
-    print(chunk)
