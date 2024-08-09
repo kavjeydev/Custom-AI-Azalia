d6873c403d97f1196661f2a36ae153deeaa28606
kavjeydev <kavin_jey@Kavins-MacBook-Pro-2.local>
2024-08-09 16:17:22 -0600

    set up django

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("wsg")
+from openai import OpenAI
+
+client = OpenAI(api_key="sk-VMbVWoS5aF48-kDBDGc4yekmznV4YQgZ-wIj3sXZAOT3BlbkFJYVrwmEFTdfLnUufSctff_T6fp3G8lkdHh4iuT1zgYA")
+completion = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {"role": "user", "content": "write a haiku about ai"}
+    ]
+)
+
+for chunk in completion:
+    print(chunk)
